b99ea66957604b56d7b54b61372b1e3928c7554a
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-05-22 21:47:25 -0400

    scroll animations and styling

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,58 @@
+/*Scroll Animations*/
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+/*Light and Dark Modes*/
+
+var toggleDay = document.getElementById("toggleDN");
+var paper = document.getElementById("paper");
+var bug = document.getElementById("bug");
+var squares = document.getElementById("squares");
+toggleDay.addEventListener('click', dayMode);
+
+console.log(toggleDay);
+
+ function dayMode() {
+  document.body.classList.toggle("light-mode");
+  console.log("clicked");
+  if (document.body.classList.contains("light-mode")){
+    toggleDay.src = "icons/icons8-moon-90.png";
+    bug.src = "icons/icons8-bug-black-48.png";
+    paper.src = "icons/icons8-paper-black-50.png";
+    squares.src = "icons/icons8-four-squares-black-50.png";
+    console.log(day);
+  } else {
+    toggleDay.src = "icons/icons8-sun.svg";
+    bug.src = "icons/icons8-bug-48.png";
+    paper.src = "icons/icons8-paper-50.png";
+    squares.src = "icons/icons8-four-squares-50.png";
+    console.log(night);
+  }
+}
+
+/*Hamburger Menu*/
+const hamMenu = document.querySelector('.ham-menu');
+console.log(hamMenu);
+const offScreenMenu = document.querySelector('.off-screen-menu');
+console.log(offScreenMenu);
+
+hamMenu.addEventListener('click', () => {
+  console.log('clicked');
+hamMenu.classList.toggle("active");
+offScreenMenu.classList.toggle("active");
+})
 
 
 /*Date Setup*/
@@ -14,14 +69,20 @@ document.body.appendChild(footer);
 
 let skills = ["HTML", "CSS", "Javascript", "GitHub", "AdobeXD", "Webflow"];
 let skillsSection = document.getElementById("skills");
-let skillsList = document.createElement("ul");
-skillsSection.appendChild(skillsList);
+let skillGrid = document.createElement("div");
+skillGrid.classList.add('skill-grid');
+
+// let skillsList = document.createElement("ul");
+// skillsSection.appendChild(skillsList);
 
 
 for (let skill of skills) {
-    let skillItem = document.createElement("li");
-    skillItem.innerText = skill; 
-    skillsList.appendChild(skillItem);
+    let skillItem = document.createElement("div");
+    let skillText = document.createElement("h3");
+    skillText.innerText = skill; 
+    skillItem.append(skillText);
+    skillGrid.append(skillItem);
+    skillsSection.append(skillGrid);
 }
 
 /* FORM */
@@ -87,15 +148,39 @@ async function getGithubRepo() {
       if (!response.ok) {
         throw new Error(response.status);  
       }
+      console.log(`This is the data you want ${data}`);
       console.log(data[0].html_url);
       console.log(data[1].html_url);
 
     const projectSection = document.getElementById("projects");
-    const projectList = projectSection.querySelector("ul");
+    const projectList = projectSection.querySelector(".projects-list");
+    console.log(projectSection);
+    
     for (let i = 0; i < data.length; i++) {
-        const project = document.createElement("li");
-        project.innerText = data[i].html_url;
-        projectList.appendChild(project);
+        let project = document.createElement("a");
+        console.log(project);
+        let repo = data[i].html_url;
+        console.log(repo)
+        project.href = repo;
+
+        function getLastSection(url) {
+          
+          url = url.replace(/\/$/, "");
+          
+        
+          var lastSlashIndex = url.lastIndexOf("/");
+          
+       
+          var lastSection = url.substring(lastSlashIndex + 1);
+          
+          return lastSection;
+      }
+        let repoText = getLastSection(repo);
+        let result = repoText.toString();
+        project.innerText = result;
+        projectList.append(project);
+        projectSection.append(projectList);
+        console.log(projectSection);
     }
 
     } catch (error) {
@@ -104,3 +189,18 @@ async function getGithubRepo() {
     } 
   }
   getGithubRepo(); 
+
+  function getLastSection(url) {
+          
+    url = url.replace(/\/$/, "");
+    
+  
+    var lastSlashIndex = url.lastIndexOf("/");
+    
+ 
+    var lastSection = url.substring(lastSlashIndex + 1);
+    
+    return lastSection;
+}
+
+  console.log(getLastSection('https://github.com/sarross88/sarah-dapkiewicz-haumea'));
